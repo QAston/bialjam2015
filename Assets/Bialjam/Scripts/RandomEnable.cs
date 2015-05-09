931ed45541cc2c6ebf65ac2d6ecef1a309e4cb6f
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2015-05-09 14:59:41 +0200

    Add wind blow object with script for random child object enable

---
+++ Assets/Bialjam/Scripts/RandomEnable.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class RandomEnable : MonoBehaviour {
+	private List<GameObject> objects;
+	public float switchTime = 5.0f;
+	private int currentIndex;
+	private float counter;
+
+	void Awake () {
+		objects = new List<GameObject> ();
+		foreach (Transform child in transform) {
+			objects.Add(child.gameObject);
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		counter = switchTime;
+		foreach (GameObject obj in objects) {
+			obj.SetActive(false);
+		}
+		currentIndex = Random.Range (0, objects.Count);
+		objects[currentIndex].SetActive (true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		counter -= Time.deltaTime;
+		if (counter < 0.0f) {
+			objects[currentIndex].SetActive(false);
+			currentIndex = Random.Range (0, objects.Count);
+			objects[currentIndex].SetActive(true);
+			counter = switchTime;
+		}
+	}
+}
