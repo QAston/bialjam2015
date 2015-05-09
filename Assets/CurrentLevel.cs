6e4aa3a67c6ab8a328fb401b77fb6ea9124bd5f7
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2015-05-09 12:31:31 +0200

    Implement GUI

---
+++ Assets/CurrentLevel.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent(typeof (Text))]
+public class CurrentLevel : MonoBehaviour {
+	Text currentLevelText;
+
+	// Use this for initialization
+	void Start () {
+		currentLevelText = GetComponent<Text> ();
+		currentLevelText.text = "Level " + Application.loadedLevelName;
+	}
+}
---
+++ Assets/RestartLevel.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartLevel : MonoBehaviour {
+	public void RestartCurrentLevel() {
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}
