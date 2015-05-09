817c10502d5cd468eede6ee637d28d18b5ce1881
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2015-05-09 17:40:54 +0200

    Implement death after fall from specified height

--- Assets/Bialjam/Scripts/CharacterBehaviour.cs
+++ Assets/Bialjam/Scripts/CharacterBehaviour.cs
@@ -10,7 +10,8 @@ public class CharacterBehaviour : MonoBehaviour {
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;  // Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[SerializeField] private bool m_AirControl = false;                 // Whether or not a player can steer while jumping;
 	[SerializeField] private LayerMask m_WhatIsGround;                  // A mask determining what is ground to the character
-	
+	private float m_MaxFallHeight = 3.0f;
+
 	private Transform m_GroundCheck;    // A position marking where to check if the player is grounded.
 	const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool m_Grounded;            // Whether or not the player is grounded.
@@ -19,6 +20,7 @@ public class CharacterBehaviour : MonoBehaviour {
 	private Animator m_Anim;            // Reference to the player's animator component.
 	private Rigidbody2D m_Rigidbody2D;
 	private bool m_FacingRight = true;  // For determining which way the player is currently facing.
+	private float lastGroundedPostion = 0f;
 
 	public void Die() {
 		IsAlive = false;
@@ -37,21 +39,31 @@ public class CharacterBehaviour : MonoBehaviour {
 		m_CeilingCheck = transform.FindChild("CeilingCheck");
 		m_Anim = GetComponent<Animator>();
 		m_Rigidbody2D = GetComponent<Rigidbody2D>();
+		lastGroundedPostion = transform.position.y;
 	}
 	
 	
 	private void FixedUpdate()
 	{
 		m_Grounded = false;
-		
+
 		// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
 		// This can be done using layers instead but Sample Assets will not overwrite your project settings.
 		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
 		for (int i = 0; i < colliders.Length; i++)
 		{
-			if (colliders[i].gameObject != gameObject)
+			if (colliders[i].gameObject != gameObject) {
 				m_Grounded = true;
+			}
 		}
+
+		if (m_Grounded) {
+			if (Mathf.Abs(transform.position.y - lastGroundedPostion) > m_MaxFallHeight) {
+				Die ();
+			}
+			lastGroundedPostion = transform.position.y;
+		}
+
 		m_Anim.SetBool("Ground", m_Grounded);
 		
 		// Set the vertical animation
