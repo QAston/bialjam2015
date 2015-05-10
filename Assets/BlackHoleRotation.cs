a93133c720be0b4fb5d0cbe6ba42c8d2342824c5
Adrian Rutkowski <adrian.rutkowsky@gmail.com>
2015-05-10 13:23:03 +0200

    Add fade to change player state

---
+++ Assets/BlackHoleRotation.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlackHoleRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		transform.Rotate(0, 4 * Time.deltaTime, 0);
+	}
+}
